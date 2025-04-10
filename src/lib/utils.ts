dd9c151ec220bec4159ce58e15677906f4001c37
4Fort <dexterfortbsilva@gmail.com>
2025-04-11 02:31:13 +0800

    Implement country search functionality and add API response handling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { ApiResponse } from "types/ApiResponse";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isErrorResponse(
+  response: ApiResponse
+): response is {
+  message: string;
+  details: string;
+  error: true;
+  statusCode: string;
+} {
+  return (response as { error: true }).error === true;
 }
